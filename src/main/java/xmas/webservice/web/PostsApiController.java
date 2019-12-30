465bc6afb1d034a02de2f0628f89417bc33d1bdc
tommy8808 <tommyguy8808@gmail.com>
2019-12-31 04:04:39 +0900

    게시판 CRUD 완료, 시큐리티 적용 준비

---
+++ src/main/java/xmas/webservice/web/PostsApiController.java
@@ -0,0 +1,36 @@
+package xmas.webservice.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import xmas.webservice.service.PostsService;
+import xmas.webservice.web.dto.PostsResponseDto;
+import xmas.webservice.web.dto.PostsSaveRequestDto;
+import xmas.webservice.web.dto.PostsUpdateRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public  Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+}
